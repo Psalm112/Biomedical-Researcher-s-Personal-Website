b00063f809cd1b31fbbff4dc0674a0f2a5433711
Samuel Oyenuga <psa8.sa@gmail.com>
2024-11-26 01:11:38 +0100

    fixed security issue

--- regular/src/firebase.js
+++ regular/src/firebase.js
@@ -12,13 +12,13 @@ import { getStorage } from "firebase/storage";
 
 // let dd = firebase.an;
 const firebaseConfig = {
-  apiKey: "AIzaSyCM6ypNJWsTPF33wsr6n1gAGqDx0haEoSQ",
-  authDomain: "oyebanjivictor-f4d86.firebaseapp.com",
-  projectId: "oyebanjivictor-f4d86",
-  storageBucket: "oyebanjivictor-f4d86.appspot.com",
-  messagingSenderId: "388301069645",
-  appId: "1:388301069645:web:8538f8df560b433db3ab04",
-  measurementId: "G-ZCJNPC70E1",
+  apiKey: process.env.REACT_APP_API_KEY,
+  authDomain: process.env.REACT_APP_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_MESSAGING_SENDER_ID,
+  appId: process.env.REACT_APP_APP_ID,
+  measurementId: process.env.REACT_APP_MEASUREMENT_ID,
 };
 
 // Initialize Firebase and Firestore
